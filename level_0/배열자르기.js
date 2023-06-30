e4a38209190667dcff40ce84ce3bd7c197f2b224
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-06-30 23:32:44 +0900

    level_0 배열 자르기

---
+++ level_0/배열자르기.js
@@ -0,0 +1,11 @@
+// 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+
+/**
+ * [1, 2, 3, 4, 5]의 1번째 인덱스 2부터 3번째 인덱스 4 까지 자른 [2, 3, 4]를 return 합니다.
+ * [1, 3, 5]의 1번째 인덱스 3부터 2번째 인덱스 5까지 자른 [3, 5]를 return 합니다.
+ */
+
+function solution(numbers, num1, num2) {
+  const answer = numbers.slice(num1, num2 + 1);
+  return answer;
+}
