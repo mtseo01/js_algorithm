d031820af1218bb58c51d932690338caed5b98a6
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-07-24 23:45:17 +0900

    level_1 가운데 글자 가져오기

---
+++ level_1/가운데_글자_가져오기.js
@@ -0,0 +1,20 @@
+//  단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+/**
+ *  s	return
+ * "abcde"	"c"
+ * "qwer"	"we"
+ */
+
+function solution(s) {
+  const index = parseInt(s.length / 2);
+  if (s.length % 2 === 0) {
+    // 짝수
+    const answer = s.slice(index - 1, index + 1);
+    return answer;
+  } else {
+    // 홀수
+    const answer = s[index];
+    return answer;
+  }
+}
