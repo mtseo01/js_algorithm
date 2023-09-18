ed79b1c42547a8d3c8e7c8ad4070a3d485d048cd
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-09-18 23:55:01 +0900

    level_1 콜라츠 추측2

--- level_1/콜라츠_추측.js
+++ level_1/콜라츠_추측.js
@@ -17,3 +17,12 @@ function solution(num) {
 
   return -1;
 }
+
+function solution2(num) {
+  let answer = 0;
+  while (num !== 1 && answer !== 500) {
+    num % 2 === 0 ? (num = num / 2) : (num = num * 3 + 1);
+    answer++;
+  }
+  return num === 1 ? answer : -1;
+}
