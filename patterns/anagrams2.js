c9e2671e3e08b25787bce7df2cac1b9b41ba9cf6
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-10-01 23:01:29 +0900

    patterns anagrams2

---
+++ patterns/anagrams2.js
@@ -0,0 +1,26 @@
+function validAnagrams(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  const lookup = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+
+console.log(validAnagrams('aab', 'adb'));
+console.log(validAnagrams('aab', 'aba'));
+console.log(validAnagrams('aab', 'affe'));
+console.log(validAnagrams('aeefff', 'affefe'));
