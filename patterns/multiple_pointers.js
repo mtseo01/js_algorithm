ed3e8d90d5b0ea9b6f5f0a883e0eb8ba255de516
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-10-02 22:35:33 +0900

    patterns multiple pointers

---
+++ patterns/multiple_pointers.js
@@ -0,0 +1,13 @@
+// O(n^2)
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+console.log(sumZero([-3, 2, 1, 0, 3])); // [ -3, 3 ]
+console.log(sumZero([-3, 2, 1, 0, 2])); // undefined
